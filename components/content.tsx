bdc4c6458e7fd82b0997ec6fd2453d8ec898910c
Aravind D <aravindrndv8@gmail.com>
2025-01-23 19:05:25 +0530

    fix: typo

--- components/content.tsx
+++ components/content.tsx
@@ -33,7 +33,7 @@ export default function Content() {
             <SidebarTrigger />
             <div id='main-content' className='py-32 lg:px-32 px-10 flex flex-col gap-5'>
                 <h1 className='text-3xl'>JLPT Vocabs!</h1>
-                <p className='text-sm'>Select upto 10 words to generate a sentence.</p>
+                <p className='text-sm'>Select upto 10 words to generate a paragraph.</p>
                 <SelectedVocabs vocabs={selectedValues} setSelectedValues={setSelectedValues} />
                 <div className='flex gap-4'>
                     <Button className='w-auto'>{buttonText}</Button>
